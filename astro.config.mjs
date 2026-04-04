2b1ea27bc57a027a4d0e6c5abb1af27beeaada65
Markus Gammersbach <mgammersbach93@gmail.com>
2026-04-04 22:18:29 +0200

    Fix site URL to production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://autorenwebseite.netlify.app',
+	site: 'https://mgammersbach.de',
 	integrations: [sitemap()],
 });
